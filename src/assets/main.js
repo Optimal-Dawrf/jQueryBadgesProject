ec5bba9639c6365ef58c63fe44c7857aa2293836
Optimal-Dawrf <jclark@Coast.local>
2017-02-03 12:49:38 +0000

    CS

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,10 @@
 $(function() {
 
-  // your code will go here
-
+  $.ajax({
+      url: 'https://www.codeschool.com/users/OptimalDwarf.json',
+      dataType: 'jsonp',
+      success: function(response) {
+        // handle response
+      }
+    });
 });
